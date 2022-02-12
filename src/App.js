a8173a0982e74bcb6f111101604278c434ab93d4
Jacek Roszak <amigo9569@gmail.com>
2022-02-12 17:33:13 +0100

    feature: App store and request helper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import StoreProvider from "./store/StoreProvider";
+
 import "./App.scss";
 
 const App = () => {
-  return ( 
-    <p>Siema mordeczki</p>
-   );
-}
- 
+  return (
+    <StoreProvider>
+      <p>Siema mordeczki</p>
+    </StoreProvider>
+  );
+};
+
 export default App;
---
+++ src/store/StoreProvider.jsx
@@ -0,0 +1,30 @@
+import React, { createContext, useEffect, useState } from "react";
+
+import request from "../helpers/request";
+
+export const StoreContext = createContext(null);
+
+const StoreProvider = ({ children }) => {
+  const [courses, setCourses] = useState([]);
+  const [user, setUser] = useState(null);
+
+  const fetchData = async () => {
+    // await wstrzyma funkcje dopóki nie będzie data
+    const { data } = await request.get("/courses");
+
+    setCourses(data.courses);
+  };
+
+  // ładowanie danych przy odpaleniu aplikacji, pusta tablica bo wykonuje się tylko raz
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <StoreContext.Provider value={(courses, setCourses, user, setUser)}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreProvider;
